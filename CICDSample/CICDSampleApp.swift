1de5e081a2e3c7e4ce42644fb92925dab0fe76a1
Dhritiman Saha <dhritiman4th@gmail.com>
2025-07-16 00:13:28 +0530

    first commit

---
+++ CICDSample/CICDSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDSampleApp.swift
+//  CICDSample
+//
+//  Created by Dhritiman Saha on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CICDSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
